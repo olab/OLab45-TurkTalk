338e9f191d366a424cf56fbf42eaa742844bc99f
Corey <reg@pathrider.ca>
2023-12-15 08:52:49 -0700

    TTalk updates

---
+++ Endpoints/MessagePayloads/NewConnectionCommand.cs
@@ -0,0 +1,35 @@
+﻿using OLab.Access.Interfaces;
+using OLab.Common.Interfaces;
+using OLab.TurkTalk.Endpoints.Utils;
+using System.Security.Claims;
+
+namespace OLab.TurkTalk.Endpoints.MessagePayloads;
+
+public class NewConnectionCommand : TTalkMessage
+{
+  private readonly IOLabAuthentication _auth;
+  public string UserKey { get; set; }
+
+  public NewConnectionCommand(
+    IOLabConfiguration configuration,
+    string connectionId,
+    IOLabAuthentication auth) : base(
+      configuration,
+      connectionId,
+      "newConnection")
+  {
+    _auth = auth;
+
+    UserKey = new UserInfoEncoder().EncryptUser(
+      Configuration.GetAppSettings().Secret,
+      _auth.Claims["id"],
+      _auth.Claims[ClaimTypes.Name],
+      _auth.Claims["name"],
+      _auth.Claims["iss"]);
+  }
+
+  public override object Arguments()
+  {
+    return this;
+  }
+}
--- Endpoints/MessagePayloads/TTalkMessage.cs
+++ Endpoints/MessagePayloads/TTalkMessage.cs
@@ -35,32 +35,3 @@ public abstract class TTalkMessage
     };
   }
 }
-
-public class NewConnectionCommand : TTalkMessage
-{
-  private readonly IOLabAuthentication _auth;
-  public string UserKey { get; set; }
-
-  public NewConnectionCommand(
-    IOLabConfiguration configuration,
-    string connectionId,
-    IOLabAuthentication auth) : base(
-      configuration,
-      connectionId,
-      "newConnection")
-  {
-    _auth = auth;
-
-    UserKey = new UserInfoEncoder().EncryptUser(
-      Configuration.GetAppSettings().Secret,
-      _auth.Claims["id"],
-      _auth.Claims[ClaimTypes.Name],
-      _auth.Claims["name"],
-      _auth.Claims["iss"]);
-  }
-
-  public override object Arguments()
-  {
-    return this;
-  }
-}
