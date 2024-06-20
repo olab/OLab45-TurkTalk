0a7bcce9eeff43838cade6e70caac8f727c72c57
Corey Wirun <reg@pathrider.ca>
2024-06-20 06:47:24 -0600

    TTalk user context fix

--- TurkTalkSvc/Services/TurkTalkUserContextService.cs
+++ TurkTalkSvc/Services/TurkTalkUserContextService.cs
@@ -41,6 +41,7 @@ public class TurkTalkUserContextService : UserContextService
     Guard.Argument(logger).NotNull(nameof(logger));
     Guard.Argument(httpContext).NotNull(nameof(httpContext));
     Guard.Argument(dbContext).NotNull(nameof(dbContext));
+
     this.configuration = configuration;
 
     this.httpContext = httpContext;
@@ -62,9 +63,23 @@ public class TurkTalkUserContextService : UserContextService
     var dict = new Dictionary<string, string>();
     foreach (var claim in jwtToken.Claims)
       dict.TryAdd(claim.Type, claim.Value);
+
     SetClaims(dict);
 
     LoadContext();
+
+    // if no role passed in, then we assume it's a local user
+    if (GroupRoles.Count() == 0 )
+    {
+      var user = userService.GetByUserName(UserName);
+      httpContext.Items["User"] = user.Username;
+      httpContext.Items["Role"] = UserGrouproles.ListToString(user.UserGrouproles.ToList());
+    }
+    else
+    {
+      httpContext.Items["Role"] = UserGrouproles.ListToString(GroupRoles.ToList());
+      httpContext.Items["User"] = UserName;
+    }
   }
 
 }
